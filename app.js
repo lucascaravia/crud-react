a8ff3104d3887829f7c4a2ec16d883b165c6bf90
lucascaravia <88347319+lucascaravia@users.noreply.github.com>
2021-08-07 12:49:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const  CreateError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const session = require('express-session');
+const {verifyUser, verifyAdmin} = require('./middlewares/auth');
+
+dotenv.config();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const registro = require('./routes/registro');
+const productos = require('./routes/productos');
+const usuarios = require('./routes/usuarios');
+const login = require('./routes/login');
+const iindex = require('./routes/admin/iindex');
+const adminUsuarios = require('./routes/admin/adminUsuarios')
+const adminProductos = require('./routes/admin/AdminProductos')
+const adminEmpleados = require('./routes/admin/adminEmpleados')
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret : 'secretPass',
+  cookie : {maxAge: null},
+  resave: true,
+  saveUninitialized : false
+}))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/registro', registro);
+app.use('/productos', productos);
+app.use('/usuarios', usuarios)
+app.use('/login', login)
+app.use ('/admin/iindex', verifyAdmin, iindex)
+app.use('/admin/adminProductos' ,adminProductos)
+app.use('/admin/adminUsuarios',adminUsuarios)
+app.use('/admin/adminEmpleados',adminEmpleados)
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next();
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
